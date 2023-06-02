0ce458263f56ce59762da2a624e8e5dee18c6c1c
Raj Negi <razznegi123457@gmail.com>
2023-06-03 00:08:11 +0400

    first commit

---
+++ double input/gates.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+float sigmoidf(float x){
+    return 1.f / (1.f + expf(-x));
+}
+
+//OR-agate
+float train[][3] = {  //training data
+    {0,0,0},
+    {1,0,1},
+    {0,1,1},
+    {1,1,1},
+};
+
+//XOR-gate
+// (x|y) & ~(x&y)
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float cost(float w1, float w2, float b){
+    float result = 0.0f;
+    for(size_t i = 0; i < train_count; i++){
+        float x1 = train[i][0];
+        float x2 = train[i][1];
+        float y = sigmoidf(x1*w1 + x2*w2 + b);
+        //measuring how well model works
+        float d = y - train[i][2]; //deviation actual answer - output answer
+        result += d*d;
+        // printf("actual: %f, expected: %f\n", y, train[i][1]);
+    }
+    result /= train_count; //calculating average if = 0 prefectly trained
+    return result;
+}
+
+
+float rand_float(void)
+{
+    return (float) rand()/ (float) RAND_MAX;
+}
+
+
+int main()
+{
+    // srand(69);
+    srand(time(0));
+    float w0 = rand_float();
+    float w1 = rand_float();
+    float w2 = rand_float();
+    float b = rand_float();
+    
+    printf("w1 = %f, w2 = %f\n", w1, w2);
+    float c = cost(w1,w2,b);
+    printf("c = %f\n", c);
+    //finding  deratives
+    float eps = 1e-2;
+    float rate = 1e-2;
+    for(size_t i = 0; i < 1000*1000; ++i)
+    {
+        float c = cost(w1,w2,b);
+        // printf("w1 = %f, w2 = %f, c = %f\n",w1,w2,c);
+        float dw1 = (cost(w1 + eps,w2,b) - c)/eps;
+        float dw2 = (cost(w1 ,w2 + eps,b) - c)/eps;
+        float db = (cost(w1 ,w2 ,b + eps) - c)/eps;
+        w1 -= rate*dw1;
+        w2 -= rate*dw2;
+        b -= rate*db;
+    }
+
+    printf("w1 = %f, w2 = %f, c = %f, b = %f\n",w1,w2,cost(w1,w2,b), b);
+
+    for(size_t i = 0; i < 2; i++){
+        for(size_t j = 0; j < 2; ++j){
+            printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2 + b));
+        }
+    }
+    
+    return 0;
+}
---
+++ double input/twice.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+float train[][2] = {  //training data
+    {0,0},
+    {1,2},
+    {2,4},
+    {3,6},
+    {4,8},
+};
+
+#define train_count sizeof(train)/sizeof(train[0])
+
+// y = x*w; x->input
+//GPT-4 -> 1 000 000 000 000 parameters our has 2
+
+float rand_float(void)
+{
+    return (float) rand()/ (float) RAND_MAX;
+}
+
+float cost(float w, float b){
+    float result = 0.0f;
+    for(size_t i = 0; i < train_count; i++){
+        float x = train[i][0];
+        float y = x*w + b;
+        //measuring how well model works
+        float d = y - train[i][1];
+        result += d*d;
+        // printf("actual: %f, expected: %f\n", y, train[i][1]);
+    }
+    result /= train_count; //calculating average if = 0 prefectly trained
+    return result;
+}
+
+int main(){
+
+    srand(69);
+    // srand(time(0));
+    //y = x*w;
+
+    float w = rand_float()*10.0f;
+    float b = rand_float()*5.0f;
+    // float w = 1;
+    printf("%f\n",w);
+
+    //we know nothing about the data set or traine model right now just guessing
+
+    float eps = 1e-3;
+    float rate = 1e-3;
+    // printf("%f\n",cost(w));
+    // printf("%f\n",cost(w + eps));
+    // printf("%f\n",cost(w + eps*2));
+
+    //if i calculate the derative of cost function i will get the direction where the function grows and i have to move to the negative or opposite
+
+    printf("%f\n",cost(w,b));
+    for(size_t i = 0; i< 5000; ++i){
+        float c = cost(w,b);
+        float dw = (cost(w + eps, b) - c)/eps;
+        float db = (cost(w , b + eps) - c)/eps;
+        // printf("%f\n",dcost);
+        w -= rate*dw;
+        b -= rate*db;
+        printf("cost = %f, w = %f, b = %f\n", cost(w,b), w, b);
+    }
+
+    printf("-----------------------------------------\n");
+    printf("w = %f, b = %f\n",w,b);
+    // printf("Hello, Seaman!");
+    return 0;
+}
